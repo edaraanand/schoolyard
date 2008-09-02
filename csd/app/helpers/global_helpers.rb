18334f925578e9a178c3b590be32da3cf083fb2f
Niket Patel <email@niket.info>
2008-09-02 12:43:58 +0530

    [NEW] added link_to_function helper method

--- csd/app/helpers/global_helpers.rb
+++ csd/app/helpers/global_helpers.rb
@@ -3,5 +3,12 @@ module Merb
     def current_user
       @current_user ||= User.find_by_id(session[:user_id])
     end
+    
+    def link_to_function(content, options)
+      unless options.is_a?(Hash)
+        options = {:action => options}
+      end
+      tag(:a, content, :href => "javascript:;", :onclick => "#{options[:action]};return false;", :title => options[:title])
+    end
   end
 end
