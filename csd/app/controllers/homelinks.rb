805d9a784f8a1613117a5f3e9bbe6b2737f244c9
Eshwar <eshwar@eshwar-laptop.(none)>
2008-09-02 21:48:54 +0530

    [fix] modified for External links

--- csd/app/controllers/homelinks.rb
+++ csd/app/controllers/homelinks.rb
@@ -2,7 +2,9 @@ class Homelinks < Application
 
   # ...and remember, everything returned from an action
   # goes to the client...
+
   def index
+    @homelinks = Homelink.find(:all)
     render
   end
   
@@ -12,15 +14,31 @@ class Homelinks < Application
   end
 
    def create
-     @homelink = Homelink.new(params[:homelink])
-      puts @homelink.inspect
-     raise "Eshwar"
-    # @homelink.save
-      redirect url(:homelinks)
-     render
+      title = params[:homelink][:title]
+      link = params[:homelink][:link]
+     
+       s = title.zip(link)
+       links = []
+       puts s.inspect
+          s.each do |l|
+	     links << Homelink.create!({:title => l[0], :link => l[1]})
+           end
+      
+        redirect url(:homelinks)
    end
     
    def edit
+      @homelinks = Homelink.find(:all)
       render
    end
+
+
+   
+
+
+
+
+
+
+
 end
