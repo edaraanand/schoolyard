6fdef67562112672c7d7f8302cb794310b713f0d
Eshwar <eshwar@eshwar-laptop.(none)>
2008-09-04 20:04:16 +0530

    [fix] provided Rspec testing for the users model based on the validations

--- csd/app/models/user.rb
+++ csd/app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   
 
   validates_presence_of :first_name, :last_name, :email
-  validates_uniqueness_of :first_name, :last_name, :email
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :allow_blank => :true
 
    def self.authenticate(email)
        self.find_by_email(email)
