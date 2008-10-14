f35568dbd89b01f2bcd95705d11a462b0ff60e24
Eshwar <eshwar@eshwar-laptop.(none)>
2008-10-14 18:49:16 +0530

    fix] for the student directory and did some modifications

--- generic/app/controllers/students.rb
+++ generic/app/controllers/students.rb
@@ -2,35 +2,42 @@ class Students < Application
 
   def index 
      @students = Student.find(:all)
-    render
+     render
   end
   
   def new
      @student = Student.new
-     @class_rooms = Classroom.find(:all)
+     @class_rooms = Classroom.find(:all, :conditions => ['class_type=?', "Classes"])
      render
   end
   
   def create
+     @class_rooms = Classroom.find(:all, :conditions => ['class_type=?', "Classes"])
      first = params[:parent][:first_name]
      last = params[:parent][:last_name]
      email = params[:parent][:email]
-     @student = Student.create(params[:student])
-     @study = Study.create({:student_id => @student.id, :classroom_id => params[:classroom_id]})
-     s = first.zip(last, email)
-     @guardians = []
-       s.each do |p|
-	  @parent = Parent.create({:first_name => p[0], :last_name => p[1], :email => p[2],
+     @student = Student.new(params[:student])
+     if @student.save
+        @study = Study.create({:student_id => @student.id, :classroom_id => params[:classroom_id]})
+        s = first.zip(last, email)
+        @guardians = []
+        s.each do |p|
+	   @parent = Parent.create({:first_name => p[0], :last_name => p[1], :email => p[2],
 		            :phone => params[:parent][:phone], :address => params[:parent][:address]  })
 	   @guardians << Guardian.create({:parent_id => @parent.id, :student_id => @student.id } )
-       end
-     redirect url(:students)
+        end
+        redirect url(:students)
+     else
+	render :new
+     end
   end
   
   def edit
      @student = Student.find(params[:id])
      @class_rooms = Classroom.find(:all)
      @parents = @student.parents
+     puts @parents.inspect
+     @p = @student.parents.find(:first)
      render
   end
   
@@ -39,19 +46,22 @@ class Students < Application
      first = params[:parent][:first_name]
      last = params[:parent][:last_name]
      email = params[:parent][:email]
+     phone = params[:parent][:phone]
+     address = params[:parent][:address]
      @student.update_attributes(params[:student])
      @study_id = Study.find_by_student_id(@student.id)
      @studies = Study.update(@study_id.id, {:student_id => @student.id, :classroom_id => params[:classroom_id]})
      @stu = @student.guardians
      guardian_id = @stu.collect{|x| x.parent_id }
      s = first.zip(last, email, guardian_id)
+     puts s.inspect
      @guardians = []
      s.each do |s|
 	if s[3].nil?
-	   @parent = Parent.create({:first_name => s[0], :last_name => s[1], :email => s[2]})
+		@parent = Parent.create({:first_name => s[0], :last_name => s[1], :email => s[2], :phone => "#{phone}", :address => "#{address}" })
            @guardians << Guardian.create({:student_id => @student.id, :parent_id => @parent.id})
         else
-	   Parent.update(s[3],{:first_name => s[0], :last_name => s[1], :email => s[2]} )   
+	   Parent.update(s[3],{:first_name => s[0], :last_name => s[1], :email => s[2], :phone => "#{phone}", :address => "#{address}" } )   
         end
      end
      redirect url(:students)
@@ -71,6 +81,5 @@ class Students < Application
   
   
 end
-
 
 
