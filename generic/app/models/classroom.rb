b0ce326e9c8a6515ad099147d02f0ee91fa396c5
Eshwar <eshwar@eshwar-laptop.(none)>
2008-10-24 19:14:38 +0530

    fix] for classrooms add more links , still css work is pending

--- generic/app/models/classroom.rb
+++ generic/app/models/classroom.rb
@@ -10,6 +10,5 @@ class Classroom < ActiveRecord::Base
 	has_many :teams, :through => :class_peoples, :source => :team
 	
 	validates_presence_of :class_name
-	#validates_presence_of :class_name, :on => :update
-	 #, :if => :class_name
+	
 end
