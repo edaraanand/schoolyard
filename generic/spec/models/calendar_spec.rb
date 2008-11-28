15b389d7b99f1148f02109f75aebbc9020aebb54
Eshwar <eshwar@eshwar-laptop.(none)>
2008-11-28 17:49:47 +0530

    fix] unit testing for calendars and solved some  bugs in that regarding date

--- generic/spec/models/calendar_spec.rb
+++ generic/spec/models/calendar_spec.rb
@@ -2,6 +2,35 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Calendar do
 
-  it "should have specs"
+ 
+  before(:each) do
+     @calendar = Calendar.new({:title => 'testing'})
+     t = Time.parse('2006/05/01')
+     @calendar.start_date = t
+     @calendar.end_date = t
+  end
+  
+  it "should be invalid without a Title" do
+     @calendar.should be_valid
+     @calendar.title = ''
+     @calendar.should_not be_valid
+     @calendar.errors.on(:title).should eql("can't be blank")
+  end
+  
+  it "should be invalid with out a Start Date" do
+     @calendar.should be_valid
+     @calendar.start_date = ''
+     @calendar.should_not be_valid
+     @calendar.errors.on(:start_date).should eql("can't be blank")
+   end    
 
+   it "should be invalid with out a End Date" do
+     @calendar.should be_valid
+     @calendar.end_date = ''
+     @calendar.should_not be_valid
+     @calendar.errors.on(:end_date).should eql("can't be blank")
+   end
+   
+       
+   
 end
