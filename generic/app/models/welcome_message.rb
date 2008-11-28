0e2b458757d1fb59fa81d499aefa1f82e709e949
Eshwar <eshwar@eshwar-laptop.(none)>
2008-11-28 17:50:32 +0530

    [fix] for unit test for welcome message and removed some unwanted code

--- generic/app/models/welcome_message.rb
+++ generic/app/models/welcome_message.rb
@@ -2,6 +2,6 @@ class WelcomeMessage < ActiveRecord::Base
       belongs_to :person
       
       validates_presence_of :content
-      validates_presence_of :access_name, :message => "Please select the access from the drop down"
+     
       
 end
