d1e36ae8a86f9044fa57fdcddc4dd4d228348608
Eshwar <eshwar@eshwar-laptop.(none)>
2008-12-03 16:06:34 +0530

    fix] deploy.rb has changed

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -5,7 +5,7 @@ set :revision, "HEAD"
 set :use_sudo, false
 set :scm_passphrase, 'eshwar'
 set :branch, 'master'
-
+set :deploy_via, :remote_cache
 
 default_run_options[:pty] = true
 set :ssh_options, { :forward_agent => true }
