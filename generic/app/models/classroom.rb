6e28180aeac9cfa75b688c6b7987c4e943bd247a
Eshwar <eshwar@eshwar-laptop.(none)>
2008-12-09 16:34:36 +0530

    fix] provided validation for class name in classrooms

--- generic/app/models/classroom.rb
+++ generic/app/models/classroom.rb
@@ -2,6 +2,7 @@ class Classroom < ActiveRecord::Base
   
   
   validates_presence_of :class_name, :if => Proc.new{|c| c.class_type != "Sports" }
+  validates_uniqueness_of :class_name
  
 	has_many :class_peoples
   has_many :people, :through => :class_peoples, :source => :person
