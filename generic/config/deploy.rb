f0b8154989583c3dbaf898c4e098f2451c84b8c0
Eshwar <eshwar@eshwar-laptop.(none)>
2008-12-22 22:41:47 +0530

    deploy file changed

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -25,20 +25,20 @@ role :db,  "schoolapp.insightmethods.com", :primary => true
 
 namespace :deploy do 
   
-  desc "Copying the database to somewhere"
-  task :before_update do
-    run "mv #{current_path}/generic/db/schoolapp_production.db #{current_path}/generic/lib"
-  end
+ # desc "Copying the database to somewhere"
+ # task :before_update do
+   # run "mv #{current_path}/generic/db/schoolapp_production.db #{current_path}/generic/lib"
+  #end
   
   desc "Change the database configuration file"
   task :after_update do
 	   run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
 	   run "rm -fr #{current_path}/generic/db"
 	   run "mkdir -p #{current_path}/generic/db"
-     run "mv #{current_path}/generic/lib/schoolapp_production.db #{current_path}/generic/db"
+     #run "mv #{current_path}/generic/lib/schoolapp_production.db #{current_path}/generic/db"
      run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
 	   run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-     #run "cd #{current_path}/generic && rake bootstrap:alerts"
+     run "cd #{current_path}/generic && rake bootstrap:alerts"
   end
   
   desc "Start Merb Instances"  
