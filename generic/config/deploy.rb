30cd3217dec35c165727517d559993a69d97e1ce
Eshwar <eshwar@eshwar-laptop.(none)>
2008-12-22 19:14:56 +0530

    changed the deploy.rb to maintain testdata

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -29,9 +29,9 @@ namespace :deploy do
   task :after_update do
 	  run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
 	  #run "rm -fr #{current_path}/generic/db"
-	  # run "mkdir -p #{current_path}/generic/db"
+	   #run "mkdir -p #{current_path}/generic/db"
 	  run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
-	  #run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
+	  run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
     #run "cd #{current_path}/generic && rake bootstrap:alerts"
   end
   
