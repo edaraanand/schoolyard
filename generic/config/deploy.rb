78ed4f91c8b0b1adefb13759d3c54962753957b0
Eshwar <eshwar@eshwar-laptop.(none)>
2008-12-22 20:22:14 +0530

    deploy.rb has changed

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -28,11 +28,11 @@ namespace :deploy do
   desc "Change the database configuration file"
   task :after_update do
 	   run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
-	    #run "rm -fr #{current_path}/generic/db"
-	    #run "mkdir -p #{current_path}/generic/db"
+	   run "rm -fr #{current_path}/generic/db"
+	   run "mkdir -p #{current_path}/generic/db"
 	   run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
 	   run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-    #run "cd #{current_path}/generic && rake bootstrap:alerts"
+     run "cd #{current_path}/generic && rake bootstrap:alerts"
   end
   
   desc "Start Merb Instances"  
