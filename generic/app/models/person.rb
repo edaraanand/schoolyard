948a2a1662a51f728168a68d3d26f2f6d0c7d3b8
Eshwar <eshwar@eshwar-laptop.(none)>
2008-12-26 20:25:38 +0530

    fix] cosmetic change in layout

--- generic/app/models/person.rb
+++ generic/app/models/person.rb
@@ -69,6 +69,9 @@ class Student < Person
      
      has_many :studies
      has_many :classrooms, :through => :studies, :source => :classroom
+     
+     has_many :ancestors
+     has_many :protectors, :through => :ancestors, :source => :protector
       
      validates_presence_of :birth_date
      validates_presence_of :address
