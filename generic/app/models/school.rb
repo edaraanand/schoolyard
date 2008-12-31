d1b1c2e2026f5c404098d5904e63bb11285bc019
Eshwar <eshwar@eshwar-laptop.(none)>
2008-12-31 17:26:03 +0530

    fix] cosmetic changes provided max length for schools and provided email format validation

--- generic/app/models/school.rb
+++ generic/app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ActiveRecord::Base
 	
       validates_presence_of :school_name, :phone, :email
-	
+	    validates_format_of :email, :with => %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,4})$}i
+      validates_length_of :phone, :is => 10, :message => 'must be 10 digits, excluding special characters such as spaces and dashes.', :if => Proc.new{|o| !o.phone.blank?}
 end
