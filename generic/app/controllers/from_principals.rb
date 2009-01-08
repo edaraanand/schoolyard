952e7f6e8f63dd851500a161404f745601d0e9cb
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-08 20:21:20 +0530

    fix] file upload for from the principal

--- generic/app/controllers/from_principals.rb
+++ generic/app/controllers/from_principals.rb
@@ -1,5 +1,6 @@
 class FromPrincipals < Application
   layout 'default'
+ 
 
   def index
      @announcements = Announcement.find(:all, :conditions => ['label=?', "from_principal"])
@@ -15,7 +16,17 @@ class FromPrincipals < Application
      @announcement = Announcement.new(params[:announcement])
      @announcement.label = "from_principal"
      if @announcement.save
-	      redirect resource(:from_principals)
+        unless params[:announcement][:attachment].empty?
+          @attachment = Attachment.create( :attachable_type => "Announcement",
+                                        :attachable_id => @announcement.id,
+                                        :filename => params[:announcement][:attachment][:filename],
+                                        :content_type => params[:announcement][:attachment][:content_type],
+                                        :size => params[:announcement][:attachment][:size]
+            )
+           File.makedirs("public/uploads/#{@attachment.id}")
+           FileUtils.mv(params[:announcement][:attachment][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
+        end
+        redirect resource(:from_principals)
      else
 	      render :new
      end
@@ -29,6 +40,15 @@ class FromPrincipals < Application
   def update
      @announcement = Announcement.find(params[:id])
      if @announcement.update_attributes(params[:announcement])
+        Attachment.delete_all(['attachable_id = ?', @announcement.id])
+           unless params[:announcement][:attachment].empty?
+               @attachment = Attachment.create( :attachable_type => "Announcement",
+                                        :attachable_id => @announcement.id,
+                                        :filename => params[:announcement][:attachment][:filename],
+                                        :content_type => params[:announcement][:attachment][:content_type],
+                                        :size => params[:announcement][:attachment][:size]
+               )
+           end
         @announcement.label = "from_principal"
         @announcement.save
         redirect resource(:from_principals)
@@ -43,7 +63,9 @@ class FromPrincipals < Application
   end
   
   def delete
-     Announcement.find(params[:id]).destroy
+     @announcement = Announcement.find(params[:id])
+     Attachment.delete_all(['attachable_id = ?', @announcement.id])
+     @announcement.destroy
      redirect resource(:from_principals)
   end
   
--- generic/app/models/announcement.rb
+++ generic/app/models/announcement.rb
@@ -1,9 +1,10 @@
 class Announcement < ActiveRecord::Base
 	
-     #include Paperclip
-    
+ 
      belongs_to :person
      validates_presence_of :title, :content
      validates_presence_of :access_name, :message => "Please select the access from the drop down", :if => :access_name
-      
+ 
+     attr_accessor :attachment
+     
 end
