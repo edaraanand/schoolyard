129079c1bb6f72201c7191b3417ec76483f63753
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-08 13:00:12 +0530

    fix for expiration date for announcements

--- generic/app/models/announcement.rb
+++ generic/app/models/announcement.rb
@@ -5,10 +5,5 @@ class Announcement < ActiveRecord::Base
      belongs_to :person
      validates_presence_of :title, :content
      validates_presence_of :access_name, :message => "Please select the access from the drop down", :if => :access_name
-     
-    # has_attached_file :file,
-                    #   :storage => :filesystem,
-                     #  :styles => { :medium => "300x300>", :thumb => "100x100>" }
-     
-    
+      
 end
