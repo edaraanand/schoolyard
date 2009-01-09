8468591e949e48578671ff907d02ed3e174751ef
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-09 14:24:48 +0530

    fix] solved the expiration issue in the home page

--- generic/app/controllers/homes.rb
+++ generic/app/controllers/homes.rb
@@ -5,7 +5,7 @@ class Homes < Application
     
   def index
     @date = Date.today
-    @announcements = Announcement.find(:all, :conditions => ["access_name = ? and approved = ? and approve_announcement = ?", 'Home Page', true, true], :limit => 2 )
+    @announcements = Announcement.find(:all, :conditions => ["access_name = ? and approved = ? and approve_announcement = ? and expiration >= ?", 'Home Page', true, true, @date], :limit => 2 )
     @from_principals = Announcement.find(:all, :conditions => ["label = ? and expiration >= ?", 'from_principal', @date], :limit => 2)
     @external_links = ExternalLink.find(:all, :conditions => ['label = ?', "Home Page"])
     render
@@ -25,6 +25,12 @@ class Homes < Application
   def help
     render :layout => 'help'
   end
+  
+  def download
+     @attachment = Attachment.find(params[:id])
+     send_file("#{Merb.root}/public/uploads/#{@attachment.id}/#{@attachment.filename}") 
+  end
+  
   
 end
 
