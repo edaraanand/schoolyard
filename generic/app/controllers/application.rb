80ae818449adde86d0616afdc7eb0ea727011521
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-16 22:52:59 +0530

    fix] finding the current school for multischool set up based on the url

--- generic/app/controllers/application.rb
+++ generic/app/controllers/application.rb
@@ -1,4 +1,20 @@
 class Application < Merb::Controller
 
- 
+  
+   before :find_school
+  
+    def find_school
+      if request.first_subdomain != nil
+         @current_school = School.find(:first, :conditions => ['subdomain = ?', request.first_subdomain ] )
+         # @current_school = School.first(:subdomain => request.first_subdomain)
+       else
+         # @current_school = School.first(:domain => request.domain)
+          @current_school = School.find(:first, :conditions => ['domain = ?', request.domain ] )
+       end
+    end
+   
+  # def find_school
+    #  @current_school = session.user.school unless session.user.blank?
+  # end
+  
 end
