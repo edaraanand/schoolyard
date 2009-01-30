fa17e8b35a88f95412e3c58072be68c770775719
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-30 13:52:33 +0530

    fix] modified the mailer functionality

--- generic/app/mailers/person_mailer.rb
+++ generic/app/mailers/person_mailer.rb
@@ -1,32 +1,40 @@
 class PersonMailer < Merb::MailController
 
-  before :find_school
+  
   
   def notify_on_event
-   
-    # use params[] passed to this controller to get data
-    # read more at http://wiki.merbivore.com/pages/mailers
-    render_mail
+     # use params[] passed to this controller to get data
+     # read more at http://wiki.merbivore.com/pages/mailers
+     render_mail
   end
   
   def sign_up
     @parent = params
+    @current_school = @parent.school
     render_mail  :html => :sign_up, :layout => nil
   end
   
   def new_password
     @parent = params
+    @current_school = @parent.school
     render_mail :html => :new_password, :layout => nil
   end
   
   def forgot_password
      @person = params
+     @current_school = @person.school
      render_mail :html => :forgot_password, :layout => nil
   end
   
   def password_staff
+     puts "Damn".inspect
      @staff = params
+     @current_school = @staff.school
+     puts @current_school.inspect
      render_mail :html => :password_staff, :layout => nil
+     puts "hiphip".inspect
   end
   
+ 
+      
 end
