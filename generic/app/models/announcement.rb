4bd9579b872e24b4526264c9000aaa75733b04ad
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-30 13:55:02 +0530

    fix] provided the validation for the expiration date in the announcement

--- generic/app/models/announcement.rb
+++ generic/app/models/announcement.rb
@@ -4,7 +4,7 @@ class Announcement < ActiveRecord::Base
      belongs_to :person
      belongs_to :school
      
-     validates_presence_of :title, :content
+     validates_presence_of :title, :content, :expiration
      validates_presence_of :access_name, :message => "Please select the access from the drop down", :if => :access_name
  
      attr_accessor :attachment
