ba6b438cee53fb8219c05d091406ed977c5b2ae9
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-30 13:49:46 +0530

    modified the application.rb file

--- generic/app/controllers/application.rb
+++ generic/app/controllers/application.rb
@@ -1,20 +1,30 @@
 class Application < Merb::Controller
 
   
-   before :find_school
-  
+    before :find_school
+    
+   # before :school
+ 
     def find_school
        if request.first_subdomain != nil
-         @current_school = School.find(:first, :conditions => ['subdomain = ?', request.first_subdomain ] )
-         # @current_school = School.first(:subdomain => request.first_subdomain)
+           @current_school = School.find(:first, :conditions => ['subdomain = ?', request.first_subdomain ] )
        else
-         # @current_school = School.first(:domain => request.domain)
-          @current_school = School.find(:first, :conditions => ['domain = ?', request.domain ] )
+           @current_school = School.find(:first, :conditions => ['domain = ?', request.domain ] )
        end
     end
    
   # def find_school
     #  @current_school = session.user.school unless session.user.blank?
   # end
+  
+    def school
+       if (request.first_subdomain != nil) && (request.first_subdomain == "admin")
+           raise "Eshwar".inspect
+       elsif  (request.first_subdomain != nil) && (request.first_subdomain != "admin")
+           raise "raja".inspect
+       else
+           raise "gouthama".inspect
+       end
+     end
   
 end
