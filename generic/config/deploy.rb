5c393eca311281dd4fd4f21f1c176288a2f06ae8
Eshwar <eshwar@eshwar-laptop.(none)>
2009-01-30 19:12:13 +0530

    fix] changed the deploy file

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -34,11 +34,11 @@ namespace :deploy do
   task :after_update do
 	   run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
 	   run "rm -fr #{current_path}/generic/db"
-     ##run "mv /home/eshwar/db  #{current_path}/generic"   
+    # run "mv /home/eshwar/db  #{current_path}/generic"   
      run "mkdir -p #{current_path}/generic/db"
      run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
 	   run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-     run "cd #{current_path}/generic && rake bootstrap:alerts" 
+     run "cd #{current_path}/generic && rake bootstrap:alerts"   
      run "cd #{current_path}/generic && rake school2:person"
   end
   
