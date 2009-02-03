32ce77d15ffe43481fb406b571f466367b616003
Eshwar <eshwar@eshwar-laptop.(none)>
2009-02-03 19:26:44 +0530

    fix] provided the validations in school.rb and modified code in person model

--- generic/app/models/school.rb
+++ generic/app/models/school.rb
@@ -15,8 +15,13 @@ class School < ActiveRecord::Base
    has_many :parents
    has_many :registrations
    
-   validates_presence_of :school_name, :phone, :email
-	 validates_format_of :email, :with => %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,4})$}i
+   validates_presence_of :school_name 
+   validates_presence_of :phone, :if => :phone
+   validates_presence_of :email, :if => :email
+	 validates_format_of :email, :with => %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,4})$}i, :if => :email
    validates_length_of :phone, :is => 10, :message => 'must be 10 digits, excluding special characters such as spaces and dashes.', :if => Proc.new{|o| !o.phone.blank?}
-      
+   validates_presence_of :subdomain, :if => :subdomain      
+   validates_uniqueness_of :school_name
+   validates_uniqueness_of :subdomain, :if => :subdomain 
+   
 end
