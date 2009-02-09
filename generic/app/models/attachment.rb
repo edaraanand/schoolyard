332075a341bba9619ef8bbc5e6246702162f47c6
Eshwar <eshwar@eshwar-laptop.(none)>
2009-02-09 16:37:36 +0530

    misc changes

--- generic/app/models/attachment.rb
+++ generic/app/models/attachment.rb
@@ -1,13 +1,9 @@
 class Attachment < ActiveRecord::Base
   
-  #has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }
- # belongs_to :form
  
-   
   def url
     "/public/uploads/#{self.id}/#{self.filename}"
   end
 
- 
-  
+   
 end                         
--- generic/schema/migrations/044_attachment_migration.rb
+++ generic/schema/migrations/044_attachment_migration.rb
@@ -4,13 +4,22 @@ class AttachmentMigration < ActiveRecord::Migration
       t.column :filename, :string
       t.column :content_type, :string
       t.column :size, :integer
+      t.column :height, :integer
+      t.column :width, :integer
+      t.column :parent_id, :integer
+      t.column :position, :integer
       t.column :attachable_id, :integer
+      t.column :thumbnail, :string
       t.column :attachable_type, :string
       t.timestamps
     end
+    add_index :attachments, :parent_id
+    add_index :attachments, [:attachable_id, :attachable_type]
   end
 
   def self.down
     drop_table :attachments
   end
 end
+
+    
