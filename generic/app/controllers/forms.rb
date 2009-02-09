13cf3397f6f54e1fd115328580d62536d1f0fa21
Eshwar <eshwar@eshwar-laptop.(none)>
2009-02-09 16:34:55 +0530

    modified the forms for file upload issues

--- generic/app/controllers/forms.rb
+++ generic/app/controllers/forms.rb
@@ -19,57 +19,88 @@ class Forms < Application
   
   def new
     @form = Form.new
+    @attachment = Attachment.new
     render
   end
   
   def create
      @form = @current_school.forms.new(params[:form])
-     if @form.save
-       unless params[:form][:attachment].empty?
-           @attachment = Attachment.create( :attachable_type => "Form",
+     i=0
+     if @form.valid?
+        unless params[:attachment]['file_'+i.to_s].empty?
+            @form.save
+            @attachment = Attachment.create( :attachable_type => "Form",
                                         :attachable_id => @form.id,
-                                        :filename => params[:form][:attachment][:filename],
-                                        :content_type => params[:form][:attachment][:content_type],
-                                        :size => params[:form][:attachment][:size]
-           )
-          File.makedirs("public/uploads/#{@attachment.id}")
-          FileUtils.mv(params[:form][:attachment][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
-       end
-       redirect resource(:forms)
-    else
-      render :new
-    end
+                                        :filename => params[:attachment]['file_'+i.to_s][:filename],
+                                        :content_type => params[:attachment]['file_'+i.to_s][:content_type],
+                                        :size => params[:attachment]['file_'+i.to_s][:size]
+             )
+              File.makedirs("public/uploads/#{@attachment.id}")
+              FileUtils.mv(params[:attachment]['file_'+i.to_s][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
+              redirect resource(:forms)
+        else
+            flash[:error] = "please upload a File"
+            render :new
+        end
+     else
+        render :new
+     end
   end
    
   def edit
-    @form = Form.find(params[:id])
-    render
+     @form = Form.find(params[:id])
+     @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
+     @allowed = 1 - @attachments.count
+     render
   end
   
   def update
-    @form = Form.find(params[:id])
-    if @form.update_attributes(params[:form])
-        Attachment.delete_all(['attachable_id = ?', @form.id])
-        unless params[:form][:attachment].empty?
-            @attachment = Attachment.create( :attachable_type => "Form",
+     @form = Form.find(params[:id])
+     @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
+     @allowed = 1 - @attachments.count
+     i=0
+     if params[:attachment]
+        if @form.update_attributes(params[:form])
+            unless params[:attachment]['file_'+i.to_s].empty?
+                @attachment = Attachment.create( :attachable_type => "Form",
                                         :attachable_id => @form.id,
-                                        :filename => params[:form][:attachment][:filename],
-                                        :content_type => params[:form][:attachment][:content_type],
-                                        :size => params[:form][:attachment][:size]
-             )
-        end
-       redirect resource(:forms)
+                                        :filename => params[:attachment]['file_'+i.to_s][:filename],
+                                        :content_type => params[:attachment]['file_'+i.to_s][:content_type],
+                                        :size => params[:attachment]['file_'+i.to_s][:size]
+                )
+                  File.makedirs("public/uploads/#{@attachment.id}")
+                  FileUtils.mv(params[:attachment]['file_'+i.to_s][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
+                  redirect resource(:forms)
+             else
+                 flash[:error] = "please upload a File"
+                 render :edit
+             end
+         else
+             render :edit
+         end
     else
-       render :edit
-    end
-    
+         if @form.update_attributes(params[:form])
+            redirect resource(:forms)
+         else
+            render :edit
+         end
+     end
   end
   
   def delete
-     @form = Form.find(params[:id])
-     Attachment.delete_all(['attachable_id = ?', @form.id])
-     @form.destroy
-     redirect resource(:forms)
+     if params[:label] == "attachment"
+        @attachment = Attachment.find(params[:id])
+        @form = Form.find_by_id(@attachment.attachable_id)
+        @attachment.destroy
+        @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
+        @allowed = 1 - @attachments.count
+        render :edit, :id => @form.id
+     else
+        @form = Form.find(params[:id])
+        Attachment.delete_all(['attachable_id = ?', @form.id])
+        @form.destroy
+        redirect resource(:forms)
+     end
   end
   
   
@@ -83,7 +114,7 @@ class Forms < Application
      @files = @current_school.forms.find(:all, :conditions => ['class_name = ?', params[:class_name] ] )
      render :layout => 'home'
   end
-  
+ 
   
   private
   
@@ -97,7 +128,7 @@ class Forms < Application
     classes = @current_school.classrooms.find(:all)
     room = classes.collect{|x| x.class_name }
     @classrooms = room.insert(0, "All Classes")
-    @years = (1999..2025).to_a
+    @years = (2009..2025).to_a
   end
   
  
@@ -115,4 +146,5 @@ class Forms < Application
      end
   end  
   
+
 end
