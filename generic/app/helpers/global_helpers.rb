1fcf831215af5b0b35048143bed1418bac3ea379
Eshwar <eshwar@eshwar-laptop.(none)>
2009-03-21 10:39:54 +0530

    fix] provided the truncation for text in home page add added helper class for that

--- generic/app/helpers/global_helpers.rb
+++ generic/app/helpers/global_helpers.rb
@@ -25,5 +25,24 @@ module Merb
            link_to(name, url)
         end
      end
+     
+     def truncate(text, *args)
+         options = args.extract_options!
+         unless args.empty?
+           ActiveSupport::Deprecation.warn('truncate takes an option hash instead of separate ' +
+             'length and omission arguments', caller)
+ 
+           options[:length] = args[0] || 30
+           options[:omission] = args[1] || "..."
+         end
+         options.reverse_merge!(:length => 30, :omission => "...")
+ 
+         if text
+           l = options[:length] - options[:omission].mb_chars.length
+           chars = text.mb_chars
+           (chars.length > options[:length] ? chars[0...l] + options[:omission] : text).to_s
+         end
+       end
+       
   end
 end
