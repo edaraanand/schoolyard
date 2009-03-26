cc60cfc4d5e7f34ef0faad1eaae418b6070241d7
Eshwar <eshwar@eshwar-laptop.(none)>
2009-03-26 20:38:09 +0530

    added pagination and added one new file for pagination

---
+++ generic/lib/base_ext.rb
@@ -0,0 +1,47 @@
+module ActiveRecord #:nodoc:
+  class Base
+    def next_id
+      @next ||= self.class.find(:first, :select => ['id'],:conditions => ["id > ? and label=?", id, "from_principal"], :order => 'id')
+      @next ? @next.id : nil
+    end
+
+    def previous_id
+      @previous ||= self.class.find(:first, :select => ['id'],:conditions => ["id < ? and label=?", id, "from_principal"], :order => "id desc")
+      @previous ? @previous.id : nil
+    end
+    
+    def next_record
+      @next ||= self.class.find(:first, :select => ['id'],:conditions => ["id > ? and access_name=?", id, "Home Page"], :order => 'id')
+      @next ? @next.id : nil
+    end
+
+    def previous_record
+      @previous ||= self.class.find(:first, :select => ['id'],:conditions => ["id < ? and access_name=?", id, "Home Page"], :order => "id desc")
+      @previous ? @previous.id : nil
+    end
+    
+    def next_class_announcement
+        @announcement = Announcement.find_by_id(id)
+        @next ||= self.class.find(:first, :select => ['id'],:conditions => ["id > ? and access_name=?", id, @announcement.access_name ], :order => 'id')
+        @next ? @next.id : nil
+    end
+
+    def previous_class_announcement
+       @announcement = Announcement.find_by_id(id)
+       @previous ||= self.class.find(:first, :select => ['id'], :conditions => ["id < ? and access_name=?", id, @announcement.access_name], :order => "id desc")
+       @previous ? @previous.id : nil
+    end
+    
+    def next_announcement
+       @next ||= self.class.find(:first, :select => ['id'],:conditions => ["id > ? and label=?", id, "staff" ], :order => 'id')
+       @next ? @next.id : nil
+    end
+
+    def previous_announcement
+       @previous ||= self.class.find(:first, :select => ['id'], :conditions => ["id < ? and label=?", id, "staff"], :order => "id desc")
+       @previous ? @previous.id : nil
+    end
+    
+    
+  end
+end
