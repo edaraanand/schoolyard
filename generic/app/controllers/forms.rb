b8ea0b8386814c60edfc7e2532f188ad392a9c62
Eshwar <eshwar@eshwar-laptop.(none)>
2009-04-24 16:37:22 +0530

    fix] used rbeautify for all the controllers for proper intendation

--- generic/app/controllers/forms.rb
+++ generic/app/controllers/forms.rb
@@ -1,12 +1,12 @@
 class Forms < Application
-  
+
   layout 'default'
   before :find_school
   before :classrooms
   before :access_rights, :exclude => [:form_files]
   before :forms, :only => [:index]
- 
-    
+
+
   def index
     if params[:class_name].nil?
       @forms = @current_school.forms.paginate(:all, :per_page => 25,  :page => params[:page])
@@ -17,155 +17,155 @@ class Forms < Application
     @files = @current_school.forms.paginate(:all, :conditions => ['class_name = ?', params[:class_name] ], :per_page => 25,  :page => params[:page] )
     render
   end
-  
+
   def new
     @form = Form.new
     @attachment = Attachment.new
     render
   end
-  
+
   def create
-     @form = @current_school.forms.new(params[:form])
-     i=0
-     if @form.valid?
-        if ( (params[:form][:class_name] != "") && (params[:form][:year] != "") )
-            unless params[:attachment]['file_'+i.to_s].empty?
-                @form.save
-                @attachment = Attachment.create( :attachable_type => "Form",
-                                        :attachable_id => @form.id,
-                                        :filename => params[:attachment]['file_'+i.to_s][:filename],
-                                        :content_type => params[:attachment]['file_'+i.to_s][:content_type],
-                                        :size => params[:attachment]['file_'+i.to_s][:size]
-                  )
-                 File.makedirs("public/uploads/#{@attachment.id}")
-                 FileUtils.mv(params[:attachment]['file_'+i.to_s][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
-                 redirect resource(:forms)
-             else
-                flash[:error] = "please upload a File"
-                render :new
-             end
+    @form = @current_school.forms.new(params[:form])
+    i=0
+    if @form.valid?
+      if ( (params[:form][:class_name] != "") && (params[:form][:year] != "") )
+        unless params[:attachment]['file_'+i.to_s].empty?
+          @form.save
+          @attachment = Attachment.create( :attachable_type => "Form",
+          :attachable_id => @form.id,
+          :filename => params[:attachment]['file_'+i.to_s][:filename],
+          :content_type => params[:attachment]['file_'+i.to_s][:content_type],
+          :size => params[:attachment]['file_'+i.to_s][:size]
+          )
+          File.makedirs("public/uploads/#{@attachment.id}")
+          FileUtils.mv(params[:attachment]['file_'+i.to_s][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
+          redirect resource(:forms)
         else
-            flash[:error] = "Please select classroom and year"
-            render :new
+          flash[:error] = "please upload a File"
+          render :new
         end
-     else
+      else
+        flash[:error] = "Please select classroom and year"
         render :new
-     end
-     
+      end
+    else
+      render :new
+    end
+
   end
-   
+
   def edit
-     @form = Form.find(params[:id])
-     @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
-     @allowed = 1 - @attachments.size
-     render
+    @form = Form.find(params[:id])
+    @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
+    @allowed = 1 - @attachments.size
+    render
   end
-  
+
   def update
-     @form = Form.find(params[:id])
-     @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
-     @allowed = 1 - @attachments.size
-     i=0
-     if params[:attachment]
-        if ( (params[:form][:class_name] != "") && (params[:form][:year] != "") )
-            if @form.update_attributes(params[:form])
-               unless params[:attachment]['file_'+i.to_s].empty?
-                    @attachment = Attachment.create( :attachable_type => "Form",
-                                        :attachable_id => @form.id,
-                                        :filename => params[:attachment]['file_'+i.to_s][:filename],
-                                        :content_type => params[:attachment]['file_'+i.to_s][:content_type],
-                                        :size => params[:attachment]['file_'+i.to_s][:size]
-                     )
-                     File.makedirs("public/uploads/#{@attachment.id}")
-                     FileUtils.mv(params[:attachment]['file_'+i.to_s][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
-                     redirect resource(:forms)
-               else
-                  flash[:error] = "please upload a File"
-                  render :edit
-               end
-            else
-               render :edit
-            end
-        else
-            flash[:error] = "Please select classroom and year"
+    @form = Form.find(params[:id])
+    @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
+    @allowed = 1 - @attachments.size
+    i=0
+    if params[:attachment]
+      if ( (params[:form][:class_name] != "") && (params[:form][:year] != "") )
+        if @form.update_attributes(params[:form])
+          unless params[:attachment]['file_'+i.to_s].empty?
+            @attachment = Attachment.create( :attachable_type => "Form",
+            :attachable_id => @form.id,
+            :filename => params[:attachment]['file_'+i.to_s][:filename],
+            :content_type => params[:attachment]['file_'+i.to_s][:content_type],
+            :size => params[:attachment]['file_'+i.to_s][:size]
+            )
+            File.makedirs("public/uploads/#{@attachment.id}")
+            FileUtils.mv(params[:attachment]['file_'+i.to_s][:tempfile].path, "public/uploads/#{@attachment.id}/#{@attachment.filename}")
+            redirect resource(:forms)
+          else
+            flash[:error] = "please upload a File"
             render :edit
+          end
+        else
+          render :edit
         end
-   else
-       if ( (params[:form][:class_name] != "") && (params[:form][:year] != "") )
-           if @form.update_attributes(params[:form])
-              redirect resource(:forms)
-           else
-              render :edit
-           end
+      else
+        flash[:error] = "Please select classroom and year"
+        render :edit
+      end
+    else
+      if ( (params[:form][:class_name] != "") && (params[:form][:year] != "") )
+        if @form.update_attributes(params[:form])
+          redirect resource(:forms)
         else
-            flash[:error] = "Please select classroom and year"
-            render :edit
-        end    
-     end
+          render :edit
+        end
+      else
+        flash[:error] = "Please select classroom and year"
+        render :edit
+      end
+    end
   end
-  
+
   def delete
-     if params[:label] == "attachment"
-        @attachment = Attachment.find(params[:id])
-        @form = Form.find_by_id(@attachment.attachable_id)
-        @attachment.destroy
-        @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
-        @allowed = 1 - @attachments.size
-        render :edit, :id => @form.id
-     else
-        @form = Form.find(params[:id])
-        Attachment.delete_all(['attachable_id = ?', @form.id])
-        @form.destroy
-        redirect resource(:forms)
-     end
+    if params[:label] == "attachment"
+      @attachment = Attachment.find(params[:id])
+      @form = Form.find_by_id(@attachment.attachable_id)
+      @attachment.destroy
+      @attachments = Attachment.find(:all, :conditions => ["attachable_id = ? and attachable_type = ?", @form.id, "Form"])
+      @allowed = 1 - @attachments.size
+      render :edit, :id => @form.id
+    else
+      @form = Form.find(params[:id])
+      Attachment.delete_all(['attachable_id = ?', @form.id])
+      @form.destroy
+      redirect resource(:forms)
+    end
   end
-  
-  
+
+
   def form_files
-     @select = "forms"
-     @selected = "all_forms"
-     unless params[:id].nil?
-         @class = @current_school.classrooms.find(params[:id])
-         @forms = @current_school.forms.paginate(:all, :conditions => ["class_name = ?", @class.class_name ], :per_page => 25,  :page => params[:page] )
-         @selected = @class.class_name 
-     end
-     if params[:l] == "all_forms"
-       @all_forms = @current_school.forms.paginate(:all, :per_page => 25,  :page => params[:page])
-     end
-     render :layout => 'directory'
+    @select = "forms"
+    @selected = "all_forms"
+    unless params[:id].nil?
+      @class = @current_school.classrooms.find(params[:id])
+      @forms = @current_school.forms.paginate(:all, :conditions => ["class_name = ?", @class.class_name ], :per_page => 25,  :page => params[:page] )
+      @selected = @class.class_name
+    end
+    if params[:l] == "all_forms"
+      @all_forms = @current_school.forms.paginate(:all, :per_page => 25,  :page => params[:page])
+    end
+    render :layout => 'directory'
   end
- 
-  
+
+
   private
-  
+
   def forms
-     @class = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
-     room = @class.collect{|x| x.class_name.titleize }
-     @classrooms = room.insert(0, "All Forms")
+    @class = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
+    room = @class.collect{|x| x.class_name.titleize }
+    @classrooms = room.insert(0, "All Forms")
   end
-  
+
   def classrooms
     classes = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
     room = classes.collect{|x| x.class_name.titleize }
     @classrooms = room.insert(0, "All Classes")
     @years = (2009..2025).to_a
   end
-  
- 
+
+
   def access_rights
-     @selected = "forms"
-     have_access = false
-     @view = Access.find_by_name('view_all')
-     @ann = Access.find_by_name('forms')
-     @access_people = session.user.access_peoples.delete_if{|x| x.access_id == @view.id }
-     @access_people.each do |f|
-       have_access = (f.all == true) || (f.access_id == @ann.id)
-       break if have_access
-     end
-     unless have_access
-       redirect resource(:homes)
-     end
-  end  
-  
+    @selected = "forms"
+    have_access = false
+    @view = Access.find_by_name('view_all')
+    @ann = Access.find_by_name('forms')
+    @access_people = session.user.access_peoples.delete_if{|x| x.access_id == @view.id }
+    @access_people.each do |f|
+      have_access = (f.all == true) || (f.access_id == @ann.id)
+      break if have_access
+    end
+    unless have_access
+      redirect resource(:homes)
+    end
+  end
+
 
 end
--- generic/app/controllers/welcome_messages.rb
+++ generic/app/controllers/welcome_messages.rb
@@ -1,127 +1,127 @@
 class WelcomeMessages < Application
- 
+
   layout 'default'
   before :find_school
   before :rooms, :only => [:new, :create, :edit, :update]
   before :access_rights
-  
-  
+
+
   def index
-       classrooms
+    classrooms
     if params[:label] == "home_messages"
-       @w_messages = @current_school.welcome_messages.find(:all, :conditions => ['access_name =?', "Home Page"], :order => "created_at DESC")
+      @w_messages = @current_school.welcome_messages.find(:all, :conditions => ['access_name =?', "Home Page"], :order => "created_at DESC")
     else
-       @welcome_messages = @current_school.welcome_messages.find(:all, :conditions => ['access_name =?', params[:access_name]], :order => "created_at DESC")
-       if params[:access_name].nil?
-          @welcome = @current_school.welcome_messages.find(:all, :order => "created_at DESC")
-       end
-       if params[:access_name] == "All Messages"
-          @all_messages = @current_school.welcome_messages.find(:all, :order => "created_at DESC")
-       end
+      @welcome_messages = @current_school.welcome_messages.find(:all, :conditions => ['access_name =?', params[:access_name]], :order => "created_at DESC")
+      if params[:access_name].nil?
+        @welcome = @current_school.welcome_messages.find(:all, :order => "created_at DESC")
+      end
+      if params[:access_name] == "All Messages"
+        @all_messages = @current_school.welcome_messages.find(:all, :order => "created_at DESC")
+      end
     end
-     render
+    render
   end
-  
+
   def new
-     @welcome_message = WelcomeMessage.new
-     render                            
+    @welcome_message = WelcomeMessage.new
+    render
   end
 
   def create
-      @welcome_message = @current_school.welcome_messages.new(params[:welcome_message])
-      if params[:welcome_message][:access_name] != ""
-         @welcome_message.person_id = @current_user.id
-         if @welcome_message.save
-	          redirect resource(:welcome_messages)
-         else
-	          render :new
-         end
+    @welcome_message = @current_school.welcome_messages.new(params[:welcome_message])
+    if params[:welcome_message][:access_name] != ""
+      @welcome_message.person_id = @current_user.id
+      if @welcome_message.save
+        redirect resource(:welcome_messages)
       else
-         flash[:error] = "Please select the option"
-         render :new
+        render :new
       end
-  end 
-  
+    else
+      flash[:error] = "Please select the option"
+      render :new
+    end
+  end
+
   def edit
-      @welcome_message = WelcomeMessage.find(params[:id])
-      ss = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
-      om = ss.collect{|x| x.class_name.titleize }
-      @test = om.insert(0, "Home Page")
-     render
+    @welcome_message = WelcomeMessage.find(params[:id])
+    ss = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
+    om = ss.collect{|x| x.class_name.titleize }
+    @test = om.insert(0, "Home Page")
+    render
   end
-  
+
   def update
-     @welcome_message = WelcomeMessage.find(params[:id])
-     if params[:welcome_message][:access_name] != ""
-        if @welcome_message.update_attributes(params[:welcome_message])
-	         @welcome_message.person_id = @current_user.id
-	         redirect resource(:welcome_messages)
-        else
-	         render :edit
-        end
-     else
-         flash[:error] = "Please select the option"
-         render :edit
-     end
-  end
-  
-  def delete
-      WelcomeMessage.find(params[:id]).destroy
-      redirect resource(:welcome_messages)
-  end
-  
-  def preview
-     if params[:welcome_message][:access_name] == "Home Page"
-        @select = "home"
-        render :layout => "home"
-     else
-        @select = "classrooms"
-        @selected = nil
-        @classroom = @current_school.classrooms.find_by_class_name(params[:welcome_message][:access_name])
-        render :layout => 'class_change', :id => @classroom.id
-     end
-  end
-   
-  private
-  
-  def classrooms
-     @class = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
-     room = @class.collect{|x| x.class_name.titleize }
-     @classrooms = room.insert(0, "All Messages")
-     @classrooms = room.insert(1, "Home Page")
-  end
-  
-  def rooms
-      @class = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
-      room = @class.collect{|x| x.class_name }
-      @classrooms = room.insert(0, "Home Page")
-      @messages = @current_school.welcome_messages.find(:all)
-      if @messages.empty?
-         @eee = @classrooms
+    @welcome_message = WelcomeMessage.find(params[:id])
+    if params[:welcome_message][:access_name] != ""
+      if @welcome_message.update_attributes(params[:welcome_message])
+        @welcome_message.person_id = @current_user.id
+        redirect resource(:welcome_messages)
       else
-         @messages.each do |f|
-           if @classrooms.include?(f.access_name)
-              @eee = @classrooms.delete_if{|x| x == f.access_name}
-           else
-              @eee =  @classrooms
-           end
-         end
+        render :edit
       end
+    else
+      flash[:error] = "Please select the option"
+      render :edit
+    end
   end
-   
-   def access_rights
-     @selected = "welcome_messages"
-     have_access = false
-     @view = Access.find_by_name('view_all')
-     @ann = Access.find_by_name('welcome_messages')
-     @access_people = session.user.access_peoples.delete_if{|x| x.access_id == @view.id }
-     @access_people.each do |f|
-       have_access = (f.all == true) || (f.access_id == @ann.id)
-       break if have_access
-     end
-     unless have_access
-       redirect resource(:homes)
-     end
-  end  
-  
+
+  def delete
+    WelcomeMessage.find(params[:id]).destroy
+    redirect resource(:welcome_messages)
+  end
+
+  def preview
+    if params[:welcome_message][:access_name] == "Home Page"
+      @select = "home"
+      render :layout => "home"
+    else
+      @select = "classrooms"
+      @selected = nil
+      @classroom = @current_school.classrooms.find_by_class_name(params[:welcome_message][:access_name])
+      render :layout => 'class_change', :id => @classroom.id
+    end
+  end
+
+  private
+
+  def classrooms
+    @class = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
+    room = @class.collect{|x| x.class_name.titleize }
+    @classrooms = room.insert(0, "All Messages")
+    @classrooms = room.insert(1, "Home Page")
+  end
+
+  def rooms
+    @class = @current_school.classrooms.find(:all, :conditions => ['activate = ?', true])
+    room = @class.collect{|x| x.class_name }
+    @classrooms = room.insert(0, "Home Page")
+    @messages = @current_school.welcome_messages.find(:all)
+    if @messages.empty?
+      @eee = @classrooms
+    else
+      @messages.each do |f|
+        if @classrooms.include?(f.access_name)
+          @eee = @classrooms.delete_if{|x| x == f.access_name}
+        else
+          @eee =  @classrooms
+        end
+      end
+    end
+  end
+
+  def access_rights
+    @selected = "welcome_messages"
+    have_access = false
+    @view = Access.find_by_name('view_all')
+    @ann = Access.find_by_name('welcome_messages')
+    @access_people = session.user.access_peoples.delete_if{|x| x.access_id == @view.id }
+    @access_people.each do |f|
+      have_access = (f.all == true) || (f.access_id == @ann.id)
+      break if have_access
+    end
+    unless have_access
+      redirect resource(:homes)
+    end
+  end
+
 end
