ee7cf75c978b621d6422421535043988b73915db
Eshwar <eshwar@eshwar-laptop.(none)>
2009-05-05 20:53:48 +0530

    fix] testing the deployment

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -4,7 +4,7 @@ set :repository,  "git@github.com:eshwardeep/schoolapp.git"
 set :branch, 'master'
 set :revision, "HEAD"
 set :domain,   "forge@174.129.200.101"
-set :deploy_to,  "/home/forge/schoolapp"
+set :deploy_to,  "/home/forge"
 set :start_port, 7001
 set :log_path, "#{shared_path}/log/production.log"
 set :adapter, 'mongrel'
@@ -15,13 +15,13 @@ namespace :vlad do
   
    desc "Change the database configuration file"
    remote_task :after_update do
-      run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
-      run "mkdir -p #{current_path}/generic/db"
-      run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
-      run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-      run "cd #{current_path}/generic && rake bootstrap:alerts"
-      run "cd #{current_path}/generic && rake contact:school"
-      run "cd #{current_path}/generic && rake admin:person"
+      #run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
+      #run "mkdir -p #{current_path}/generic/db"
+      #run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
+      #run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
+      #run "cd #{current_path}/generic && rake bootstrap:alerts"
+      #run "cd #{current_path}/generic && rake contact:school"
+      #run "cd #{current_path}/generic && rake admin:person"
    end
     
    desc "Start Merb Instances"  
