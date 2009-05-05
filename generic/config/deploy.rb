2a3a8a97567bb5fd75e6c157453ba7162e6a6e92
Eshwar <eshwar@eshwar-laptop.(none)>
2009-05-05 21:00:25 +0530

    fix] misc

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -15,13 +15,13 @@ namespace :vlad do
   
    desc "Change the database configuration file"
    remote_task :after_update do
-      #run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
-      #run "mkdir -p #{current_path}/generic/db"
-      #run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
-      #run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-      #run "cd #{current_path}/generic && rake bootstrap:alerts"
-      #run "cd #{current_path}/generic && rake contact:school"
-      #run "cd #{current_path}/generic && rake admin:person"
+      run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
+      run "mkdir -p #{current_path}/generic/db"
+      run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
+      run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
+      run "cd #{current_path}/generic && rake bootstrap:alerts"
+      run "cd #{current_path}/generic && rake contact:school"
+      run "cd #{current_path}/generic && rake admin:person"
    end
     
    desc "Start Merb Instances"  
