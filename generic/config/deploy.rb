5c101639b2b8295311343e6c02b18b4758c13f5a
Eshwar <eshwar@eshwar-laptop.(none)>
2009-05-06 16:26:16 +0530

    fix] changed deploy file

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -8,34 +8,17 @@ set :processes, 1
 
 
 
-#namespace :vlad do
+namespace :vlad do
   
- #  desc "Change the database configuration file"
- #  remote_task :after_update do
- #     run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
- #     run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
- #     run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
- #     run "cd #{current_path}/generic && rake bootstrap:alerts"
- #     run "cd #{current_path}/generic && rake contact:school"
- #     run "cd #{current_path}/generic && rake admin:person"
- #  end
- #   
- #  desc "Start Merb Instances"  
- #  remote_task :start do 
-	#    run "merb -m #{deploy_to}/current -e production -c #{processes} --port #{start_port} -m #{current_path}/generic -L #{log_path}"  
- #  end
- #  
- #  desc "Stop Merb Instances"
- #  remote_task :stop do
- #     run "merb -m #{deploy_to}/current -K all"
- #  end
- #  
- #  desc "Deploying the application"
- #  remote_task :deploy do
- #     Rake::Task['vlad:update'].invoke
- #     Rake::Task['vlad:after_update'].invoke
- #     Rake::Task['vlad:start'].invoke
- #     Rake::Task['vlad:stop'].invoke
- #  end
+     desc 'Runs vlad:update, vlad:symlink, vlad:migrate and vlad:start'
+     task :deploy => ['vlad:update', 'vlad:symlink', 'vlad:migrate', 'vlad:stop_app', 'vlad:start_app']
      
-#end
+     desc 'Symlinks your custom directories'
+     remote_task :symlink, :roles => :app do
+       run "ln -s #{shared_path}/database.yml #{current_release}/config/database.yml"
+     end
+     
+end
+
+
+
