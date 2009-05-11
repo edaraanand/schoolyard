8b18937468da8863f8034fc969ce89465ae6ecb7
Eshwar <eshwar@eshwar-laptop.(none)>
2009-05-11 13:15:39 +0530

    fix] modified deploy.rb to make a build in old server i.e. schoolyardapp.net

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -1,86 +1,65 @@
 set :application, "schoolapp"
-set :user, "forge"
-set :domain, "#{user}@schoolyardapp.com"
-set :deploy_to, "/home/forge/#{application}"
+set :scm, "git"
 set :repository,  "git@github.com:eshwardeep/schoolapp.git"
 set :revision, "HEAD"
-set :branch, "master"
-set :adapter, "mongrel"
-#set :start_port, 7001
+set :use_sudo, false
+set :scm_passphrase, 'eshwar'
+set :branch, 'master'
+set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true
+set :ssh_options, { :forward_agent => true }
+#set :user, 'eshwar'
+
+#set :domain,   "sdb.schoolyardapp.net"
+set :domain,   "eshwar@sdb.schoolyardapp.net"
+set :deploy_to,  "/home/eshwar/schoolapp"
+set :adapter, 'mongrel' # or 'thin' 
+set :start_port, 7001
 set :processes, 1
 set :log_path, "#{shared_path}/log/production.log"
 
-namespace :vlad do
-    Rake.clear_tasks('vlad:start_app', 'vlad:migrate', 'vlad:update_symlinks')
-    
-# # Merb app server
+role :app, "sdb.schoolyardapp.net"  
+role :web, "sdb.schoolyardapp.net"
+role :db,  "sdb.schoolyardapp.net", :primary => true
 
-   #set :merb_address,       "127.0.0.1"
-   #set :merb_clean,         false
-   #set :merb_command,       'merb'
-   #set :merb_conf,          nil
-   #set :merb_extra_config,  nil
-   #set :merb_environment,   "production"
-   #set :merb_group,         nil
-   #set :merb_port,          7001
-   #set :merb_prefix,        nil
-   #set :merb_servers,       1
-   #set :merb_user,          nil
+
+namespace :deploy do 
   
-   desc "Prepares application servers for deployment. merb
-        configuration is set via the merb_* variables.".cleanup
-   remote_task :setup_app, :roles => :app do
-     "rake"
-   end
-   
-  # def merb(cmd) # :nodoc:
-  #   "cd #{current_path} && #{merb_command} -p #{merb_port} -c #{merb_servers} -e #{merb_environment} #{cmd}"
-  # end --port #{start_port}
-   
-   desc "Restart the app servers"
-   remote_task :start_app, :roles => :app do
-      run "merb -a #{adapter} -e production -c #{processes} -m #{current_path}/generic -L #{log_path}"
-   end
-   
-   remote_task :start_app => :stop_app
-
-   desc "Stop the app servers"
-   remote_task :stop_app, :roles => :app do
-     #run merb("-K all")
-     run "cd #{current_path} && merb -a #{adapter} -K all" 
-   end
-   
-   desc "updates the code and changing symlink files"
-   remote_task :update, :roles => :app do
-      run "mkdir -p #{current_path}/generic/db"
-      run "mv #{latest_release}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
-      run "cp #{latest_release}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
-   end
-   
-   desc "Symlink configuration".cleanup
-   remote_task :update_symlinks, :roles => :app do
-     run [ "ln -s #{shared_path}/log #{latest_release}/log",
-           "ln -s #{shared_path}/system #{latest_release}/generic/public/system",
-           "ln -s #{shared_path}/pids #{latest_release}/tmp/pids" ].join(" && ")
-   end
-   
-   desc "Running migrations with some rake tasks for production"
-   remote_task :migrate, :roles => :app do
-      run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-      run "cd #{current_path}/generic && rake bootstrap:alerts"
-      run "cd #{current_path}/generic && rake contact:school"
-      run "cd #{current_path}/generic && rake admin:person"
+   desc "Copying the database to somewhere"
+   task :before_update do
+     run "mv #{current_path}/generic/db  /home/eshwar"
    end
   
-   desc "Full deployment cycle"
-   remote_task :deploy, :roles => :app do
-      Rake::Task['vlad:update'].invoke
-      Rake::Task['vlad:migrate'].invoke
-      Rake::Task['vlad:start_app'].invoke
-      Rake::Task['vlad:cleanup'].invoke
-   end
+  desc "Change the database configuration file"
+  task :after_update do
+	   run "mv #{current_path}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
+	  # run "rm -fr #{current_path}/generic/db"
+     run "mv /home/eshwar/db  #{current_path}/generic"   
+     #run "rm -fr #{current_path}/generic/schema/schema.rb"
+    # run "mkdir -p #{current_path}/generic/db"
+     run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
+     run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
+     #run "cd #{current_path}/generic && rake contact:school"
+     #run "cd #{current_path}/generic && rake bootstrap:alerts"  
+     #run "cd #{current_path}/generic && rake admin:person"
+  end
+  
+  desc "Start Merb Instances"  
+  task :start do 
+	  run "merb -a #{adapter} -e production -c #{processes} --port #{start_port} -m #{current_path}/generic -L #{log_path}"  
+  end
+ 
+  desc "Stop Merb Instances"  
+  task :stop do 
+    run "cd #{current_path} && merb -a #{adapter} -K all"  
+  end 
+ 
+  desc "Custom restart task for Merb"
+  task :restart, :roles => :app do 
+    deploy.stop 
+    deploy.start 
+  end 
   
 end
-
-
 
