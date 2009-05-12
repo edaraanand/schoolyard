d2e0a8f265db48ae6c1fa2b51b6d983b0db5e787
Eshwar <eshwar@eshwar-laptop.(none)>
2009-05-12 13:37:17 +0530

    fix] modified Extracirrcular to "Subject" in classrooms

--- generic/app/controllers/classrooms.rb
+++ generic/app/controllers/classrooms.rb
@@ -269,7 +269,7 @@ class Classrooms < Application
     a = []
     type1 = a.insert(0, "Classes")
    # type2 = a.insert(1, "Sports")
-    @class_types = a.insert(1, "Extra Curricular")
+    @class_types = a.insert(1, "Subject")
   end
 
 
