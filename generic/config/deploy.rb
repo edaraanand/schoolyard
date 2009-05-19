c9b178bc50c9188fa4cb0ff1bb3b682d8d66cce6
Eshwar <eshwar@eshwar-laptop.(none)>
2009-05-19 11:21:04 +0530

    fix] changed deploy file

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -40,6 +40,7 @@ namespace :vlad do
       run "mv #{latest_release}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
       run "mv /home/forge/db  #{current_path}/generic" 
       run "cp #{latest_release}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
+      #run "cd #{current_path}/generic && rake feedback:person"
    end
    
    desc "Symlink configuration".cleanup
