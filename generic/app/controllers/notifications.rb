c3154b3ed23bdc74e09083574c6e3311e446290d
Eshwar <eshwar@eshwar-laptop.(none)>
2009-06-05 12:56:02 +0530

    changed to twilio account details to mine for testing logs

--- generic/app/controllers/notifications.rb
+++ generic/app/controllers/notifications.rb
@@ -1,12 +1,12 @@
    require "twiliorest.rb"
   
    # your Twilio authentication credentials   (Eshwar's Twilio Account )
-  # ACCOUNT_SID = 'ACa1b7ee5abe50974956bda599447b1f04'  
-  # ACCOUNT_TOKEN = '208232dfbc82563e6c2f9cdc54cafbe5' 
+   ACCOUNT_SID = 'ACa1b7ee5abe50974956bda599447b1f04'  
+   ACCOUNT_TOKEN = '208232dfbc82563e6c2f9cdc54cafbe5' 
   
   # Brian's Twilio Account Details
-   ACCOUNT_SID = 'ACaccbef6e62668da72003aea3ec585f89'
-   ACCOUNT_TOKEN = '169a19fa5941cd4d002231beaa870b0b'
+  # ACCOUNT_SID = 'ACaccbef6e62668da72003aea3ec585f89'
+  # ACCOUNT_TOKEN = '169a19fa5941cd4d002231beaa870b0b'
    
    # Twilio REST API version
    API_VERSION = '2008-08-01'
@@ -20,13 +20,14 @@
    #https://username:password@api.twilio.com/2008-08-01 ..
     
    # Outgoing Caller ID you have previously validated with Twilio  
-   # CALLER_ID = '(415) 287-0729'  
-     CALLER_ID = '530 554 1373'
+     CALLER_ID = '(415) 287-0729'  
+    # CALLER_ID = '530 554 1373'
    # CALLED = '530 554 1373' Myself
    
    # CALLED = '571 332 0672' AJ
    # CALLED = ' 530-756-8158' Brian
    # CALLED = '+14152870729' Niket
+     CALLED = '530 554 1373'
 
 class Notifications < Application
    layout 'default'
@@ -75,20 +76,30 @@ class Notifications < Application
   def makecall(id)
     @announcement = @current_school.announcements.find_by_id(id)
     @people = @current_school.people.find(:all)
-    @people.each do |f|
-        unless f.voice_alert.blank?
-             begin  
-                 account = TwilioRest::Account.new(ACCOUNT_SID, ACCOUNT_TOKEN)
-                 resp =  account.request( "/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/Calls", 'POST',
-                                               d = { 'Caller' => CALLER_ID,
-                                                     'Called' => "#{f.voice_alert}",
-                                                     'Url' => BASE_URL + "/reminder?id=#{@announcement.id}" }  )
-                 resp.error! unless resp.kind_of? Net::HTTPSuccess  
-             rescue StandardError => bang
-                 return  
-             end  
-        end
-    end
+   # @people.each do |f|
+   #     unless f.voice_alert.blank?
+   #          begin  
+   #              account = TwilioRest::Account.new(ACCOUNT_SID, ACCOUNT_TOKEN)
+   #              resp =  account.request( "/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/Calls.csv", 'POST',
+   #                                            d = { 'Caller' => CALLER_ID,
+   #                                                  'Called' => "#{f.voice_alert}",
+   #                                                  'Url' => BASE_URL + "/reminder?id=#{@announcement.id}" }  )
+   #              resp.error! unless resp.kind_of? Net::HTTPSuccess  
+   #          rescue StandardError => bang
+   #              return  
+   #          end  
+   #     end
+   # end
+      begin  
+          account = TwilioRest::Account.new(ACCOUNT_SID, ACCOUNT_TOKEN)
+          resp =  account.request( "/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/Calls.csv", 'POST',
+                                        d = { 'Caller' => CALLER_ID,
+                                              'Called' => CALLED,
+                                              'Url' => BASE_URL + "/reminder?id=#{@announcement.id}" }  )
+          resp.error! unless resp.kind_of? Net::HTTPSuccess  
+      rescue StandardError => bang
+          return  
+      end  
       redirect resource(:notifications)
   end
    
