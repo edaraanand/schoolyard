eb992c80c120ae71c462490faf9ab37c587491d1
Eshwar <eshwar@eshwar-laptop.(none)>
2009-06-23 13:20:41 +0530

    fix] added some views and modified views for grades

--- generic/app/models/assignment.rb
+++ generic/app/models/assignment.rb
@@ -2,6 +2,9 @@ class Assignment < ActiveRecord::Base
   
     belongs_to :category
     belongs_to :school
+    belongs_to :student
+    
+    has_many :grades
     
     # validates_presence_of :name, :date, :max_point
     #validates_numericality_of :max_point
