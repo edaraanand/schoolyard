810f3a6cd6fbc6619c6c9cb16b391a5869bc90be
Eshwar <eshwar@eshwar-laptop.(none)>
2009-06-23 13:21:41 +0530

    fix] Change in rake file

--- generic/config/environments/development.rb
+++ generic/config/environments/development.rb
@@ -3,6 +3,13 @@ Merb::Config.use { |c|
   c[:exception_details] = true
   c[:reload_classes] = true
   c[:reload_templates] = true
-  c[:reload_time] = 0.5
-  c[:log_auto_flush ] = true
+ # c[:reload_time] = 0.5
+  #c[:log_auto_flush ] = true
+  #c[:ignore_tampered_cookies] = true
+  c[:log_level] = :debug
+  #
+  #c[:log_stream] = STDOUT
+  c[:log_file]   = nil
 }
+
+
