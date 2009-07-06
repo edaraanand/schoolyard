3f1880eef5e5cd143d5c63ce766f7380cf09114c
Eshwar <raja@eshwars-mac-mini.local>
2009-07-06 22:55:59 +0530

    fix] changed deploy file

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -38,10 +38,10 @@ namespace :vlad do
    desc "updates the code and changing symlink files"
    remote_task :update, :roles => :app do
       run "mv #{latest_release}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
-      run "rm -fr #{current_path}/generic/db"
-      run "mkdir -p #{current_path}/generic/db"
+      # run "rm -fr #{current_path}/generic/db"
+      # run "mkdir -p #{current_path}/generic/db"
+      run "mv /home/forge/db  #{current_path}/generic" 
       run "cp #{latest_release}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
-     # run "mv /home/forge/db  #{current_path}/generic" 
    end
    
    desc "Symlink configuration".cleanup
@@ -54,14 +54,14 @@ namespace :vlad do
    desc "Running migrations with some rake tasks for production"
    remote_task :migrate, :roles => :app do
       run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-      run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-      run "cd #{current_path}/generic && rake bootstrap:alerts"
-      run "cd #{current_path}/generic && rake admin:person"
+    # run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
+   #  run "cd #{current_path}/generic && rake bootstrap:alerts"
+   #  run "cd #{current_path}/generic && rake admin:person"
    end
   
    desc "Full deployment cycle"
    remote_task :deploy, :roles => :app do
-     # Rake::Task['vlad:before_update'].invoke
+      Rake::Task['vlad:before_update'].invoke
       Rake::Task['vlad:update'].invoke
       Rake::Task['vlad:migrate'].invoke
       Rake::Task['vlad:start_app'].invoke
