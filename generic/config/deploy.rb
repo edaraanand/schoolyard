f1ae20ecfd366fce669b01cceacc5e11c8bc1b90
Eshwar <raja@eshwars-mac-mini.local>
2009-07-25 12:30:45 +0530

    fix] deploy file changed

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -12,7 +12,8 @@ set :deploy_via, :remote_cache
 default_run_options[:pty] = true
 set :ssh_options, { :forward_agent => true }
  
-set :domain, "eshwar@sdb.schoolyardapp.net"
+set :domain, "sdb.schoolyardapp.net"
+set :user, "eshwar"
 set :deploy_to, "/home/eshwar/schoolapp"
 set :adapter, 'mongrel' # or 'thin'
 set :start_port, 7001
@@ -40,10 +41,10 @@ namespace :deploy do
      run "cp #{current_path}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
      run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
      run "cd #{current_path}/generic && rake basecamp:notify_new_build"
-     run "cd #{current_path}/generic && rake bootstrap:student"
      #run "cd #{current_path}/generic && rake bootstrap:alerts"
      #run "cd #{current_path}/generic && rake contact:school"
      #run "cd #{current_path}/generic && rake admin:person"
+     #run "cd #{current_path}/generic && rake bootstrap:student"
   end
   
   desc "Start Merb Instances"
