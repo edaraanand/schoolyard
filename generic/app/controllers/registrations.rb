8de3f299927f6a690d8ce81d8ae824e96a4f9356
Eshwar <raja@eshwars-mac-mini.local>
2009-08-12 11:36:12 +0530

    fix] finding by password reset key i resetting the password

--- generic/app/controllers/registrations.rb
+++ generic/app/controllers/registrations.rb
@@ -227,7 +227,7 @@ class Registrations < Application
 
   def password_sent
     id = params[:id]
-    @person = @current_school.people.find(:first, :conditions => ['password_reset_key = ?', id])
+    @person = @current_school.people.find_by_password_reset_key(params[:id])
     render :layout => 'login'
   end
 
@@ -239,13 +239,11 @@ class Registrations < Application
     else
        flash[:success] = "Your password has been saved. please enter your mail-ID and password to start using SchoolYard"
        redirect url(:login)
-       #raise NotFound
     end
   end
 
   def reset_password_update
-    id = params[:id]
-    @person = @current_school.people.find(:first, :conditions => ['password_reset_key = ?', id])
+    @person = @current_school.people.find_by_password_reset_key(params[:id])
     if ( !params[:person][:password].blank? ) 
       if ( params[:person][:password] == params[:person][:password_confirmation] )
         @person.password = params[:person][:password]
