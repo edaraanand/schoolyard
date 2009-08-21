f1f2c6ae3c012eb65f0a57a7ff05827eac9c1fb9
Eshwar <raja@eshwars-mac-mini.local>
2009-08-21 21:45:35 +0530

    fix] added description field for forms

--- generic/schema/schema.rb
+++ generic/schema/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 79) do
+ActiveRecord::Schema.define(:version => 80) do
 
   create_table "access_peoples", :force => true do |t|
     t.integer  "access_id"
@@ -175,6 +175,7 @@ ActiveRecord::Schema.define(:version => 79) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "school_id"
+    t.text     "description"
   end
 
   create_table "grades", :force => true do |t|
