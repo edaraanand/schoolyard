7e3bbbe3ff82208a838c41567be3789f6501ffe9
Eshwar <raja@eshwars-mac-mini.local>
2009-08-27 17:24:11 +0530

    fix] added osme change for urgent announcements

--- generic/config/deploy.rb
+++ generic/config/deploy.rb
@@ -1,16 +1,16 @@
-set :application, "schoolapp"
-set :user, "forge"
-set :domain, "#{user}@schoolyardapp.com"
-set :deploy_to, "/home/forge/#{application}"
+set :domain, "schoolyard@dev-schoolyardapp.info"
+set :deploy_to, "/home/schoolyard/schoolapp"
 set :repository,  "git@github.com:eshwardeep/schoolapp.git"
-set :revision, "HEAD"
-set :branch, "release"
+#set :revision, "HEAD"
+set :revision, "origin/release1b"
+#set :branch, "release1b"
 set :adapter, "mongrel"
+set :port, 5001
 set :processes, 1
 set :log_path, "#{shared_path}/log/production.log"
-
+ 
 namespace :vlad do
-    Rake.clear_tasks('vlad:start_app', 'vlad:migrate', 'vlad:update_symlinks')
+   Rake.clear_tasks('vlad:start_app', 'vlad:migrate', 'vlad:update_symlinks')
     
    desc "Prepares application servers for deployment. merb
         configuration is set via the merb_* variables.".cleanup
@@ -20,11 +20,11 @@ namespace :vlad do
    
    desc "Restart the app servers"
    remote_task :start_app, :roles => :app do
-      run "merb -a #{adapter} -e production -c #{processes} -m #{current_path}/generic -L #{log_path}"
+      run "merb -a #{adapter} -e production -p #{port} -c #{processes} -m #{current_path}/generic -L #{log_path}"
    end
    
    remote_task :start_app => :stop_app
-
+ 
    desc "Stop the app servers"
    remote_task :stop_app, :roles => :app do
      run "cd #{current_path} && merb -a #{adapter} -K all" 
@@ -32,17 +32,15 @@ namespace :vlad do
    
    desc "moving the database to some other place"
    remote_task :before_update, :roles => :app do
-     run "mv #{current_path}/generic/db  /home/forge"
-     run "mv #{current_path}/generic/public/uploads /home/forge"
+       run "mv #{current_path}/generic/db  /home/schoolyard"
+     #  run "mv #{current_path}/generic/public/uploads /home/forge/info"
    end
    
    desc "updates the code and changing symlink files"
    remote_task :update, :roles => :app do
       run "mv #{latest_release}/generic/config/database.yml.production #{current_path}/generic/config/database.yml"
-      # run "rm -fr #{current_path}/generic/db"
-      # run "mkdir -p #{current_path}/generic/db"
-      run "mv /home/forge/db  #{current_path}/generic" 
-      run "mv /home/forge/uploads #{current_path}/generic/public"
+      run "mv /home/schoolyard/db  #{current_path}/generic" 
+      # run "mv /home/forge/info/uploads #{current_path}/generic/public"
       run "cp #{latest_release}/generic/lib/constantz.rb.sample #{current_path}/generic/lib/constantz.rb"
    end
    
@@ -55,12 +53,13 @@ namespace :vlad do
    
    desc "Running migrations with some rake tasks for production"
    remote_task :migrate, :roles => :app do
+      # run "mkdir -p #{current_path}/generic/db"
+      # run "mkdir -p #{current_path}/generic/tmp"
       run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
+      # run "cd #{current_path}/generic && rake bootstrap:person"
+      # run "cd #{current_path}/generic && rake admin:person"
+      # run "cd #{current_path}/generic && rake bootstrap:student_activate"
       run "cd #{current_path}/generic && rake basecamp:notify_new_build"
-      # run "cd #{current_path}/generic && rake bootstrap:student"
-      # run "cd #{current_path}/generic && rake db:migrate MERB_ENV=production"
-      #  run "cd #{current_path}/generic && rake bootstrap:alerts"
-      #  run "cd #{current_path}/generic && rake admin:person"
    end
   
    desc "Full deployment cycle"
@@ -73,6 +72,3 @@ namespace :vlad do
    end
   
 end
-
-
-
