8f45c2f10720b0596e666e8284110b865624c3b2
Eshwar <raja@eshwars-mac-mini.local>
2009-08-31 18:47:29 +0530

    [fix] added a validation for image content type in spot light model

--- app/models/spot_light.rb
+++ app/models/spot_light.rb
@@ -5,9 +5,21 @@ class SpotLight < ActiveRecord::Base
   belongs_to :school
 
   validates_presence_of :student_name, :last_name, :content
-
+ 
   def name
     (student_name and last_name) ? student_name + ' ' + last_name : ''
+  end
+  
+  def validate
+     if self.class_name == ""
+        self.errors.add("please", "select the option")
+     end
+     if self.image != ""
+       @content_types = ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png']
+       unless @content_types.include?(self.image[:content_type])
+         self.errors.add("please", "upload a image")
+       end 
+     end
   end
 
 
