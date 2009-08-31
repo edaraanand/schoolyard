9669b9571e6d82a614a033544d7d236bf1cde162
Brian <brian@insightmethods.com>
2009-08-30 18:00:08 -0700

    [MISC]

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,50 +33,49 @@ Rake.clear_tasks('vlad:stop', 'vlad:start', 'vlad:migrate')
  
 namespace :vlad do
 
-    def stop
-      run "cd #{current_path} && merb -e #{merb_env} -K all"
-    end
+  def stop
+    run "cd #{current_path} && merb -e #{merb_env} -K all"
+  end
 
-    def start
-      run "cd #{current_path} && merb -e #{merb_env} -p #{port} -m #{current_path} -c #{processes}"
-    end
+  def start
+    run "cd #{current_path} && merb -e #{merb_env} -p #{port} -m #{current_path} -c #{processes}"
+  end
 
-    desc 'restart merb'
-    remote_task :start, :roles => :app do
-      stop
-      start
-    end
+  desc 'restart merb'
+  remote_task :start, :roles => :app do
+    stop
+    start
+  end
 
-    desc 'Copy production files'
-    remote_task :before_update, :roles => :app do
-      run "tar zcf backup_before_update_#{Date.today.year}#{Date.today.month}#{Date.today.day}.tar #{deploy_to}"
-    end
+  desc 'Copy production files'
+  remote_task :before_update, :roles => :app do
+    run "tar zcf backup_before_update_#{merb_env}_#{Date.today.year}#{Date.today.month}#{Date.today.day}.tar #{deploy_to}"
+  end
 
 
-    desc 'update application rsync important files'
-    remote_task :deploy, :roles => :app do
-      Rake::Task["vlad:update"].invoke
-      Rake::Task['vlad:copy_production_files'].invoke
-      Rake::Task['vlad:copy_data_files'].invoke
-      #Rake::Task['vlad:native_gems'].invoke
-      Rake::Task["vlad:start"].invoke
-    end
+  desc 'update application rsync important files'
+  remote_task :deploy, :roles => :app do
+    Rake::Task["vlad:update"].invoke
+    Rake::Task['vlad:copy_production_files'].invoke
+    Rake::Task['vlad:copy_data_files'].invoke
+    #Rake::Task['vlad:native_gems'].invoke
+    Rake::Task["vlad:start"].invoke
+  end
 
-    desc 'Copy production files'
-    remote_task :copy_data_files, :roles => :app do
-      run "cp #{releases_path}/#{releases[releases.size - 2]}/db/production.sqlite3 #{current_path}/db/production.sqlite3"
-      run "cp -r #{releases_path}/#{releases[releases.size - 2]}/public/site_doc #{current_path}/public"
-    end
+  desc 'Copy production files'
+  remote_task :copy_data_files, :roles => :app do
+    run "cp #{releases_path}/#{releases[releases.size - 2]}/db/production.sqlite3 #{current_path}/db/production.sqlite3"
+    run "cp -r #{releases_path}/#{releases[releases.size - 2]}/public/uploads #{current_path}/public"
+  end
 
-    desc 'Copy production files'
-    remote_task :copy_production_files, :roles => :app do
-      rsync "#{Merb.root}/lib/constantz.rb.#{merb_env}", "#{current_path}/lib/constantz.rb"
-      rsync "#{Merb.root}/config/database.yml.#{merb_env}", "#{current_path}/config/database.yml"
-    end
+  desc 'Copy production files'
+  remote_task :copy_production_files, :roles => :app do
+    rsync "#{Merb.root}/lib/constantz.rb.#{merb_env}", "#{current_path}/lib/constantz.rb"
+    rsync "#{Merb.root}/config/database.yml.#{merb_env}", "#{current_path}/config/database.yml"
+  end
 
-    desc "migrate app with merb env"
-    remote_task :migrate, :roles => :app do
-      run "cd #{current_path} && rake db:migrate MERB_ENV=#{merb_env}"
-    end
-
+  desc "migrate app with merb env"
+  remote_task :migrate, :roles => :app do
+    run "cd #{current_path} && rake db:migrate MERB_ENV=#{merb_env}"
+  end
 end
--- lib/tasks/vlad.rake
+++
@@ -1,7 +0,0 @@
-begin
-  require 'rubygems'
-  require 'vlad'
-  Vlad.load(:app => :passenger, :scm => :git )
-rescue LoadError
-  puts 'Could not load Vlad'
-end
