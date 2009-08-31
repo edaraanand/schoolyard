ddba111d66363c302687426e306a5e12c6a5f5e3
Eshwar <raja@eshwars-mac-mini.local>
2009-08-31 20:23:39 +0530

    fix] done spacing in production branch in home works

--- app/controllers/home_works.rb
+++ app/controllers/home_works.rb
@@ -197,7 +197,7 @@ class HomeWorks < Application
         con = con.gsub("’", "")
         con = con.gsub("– ", "")
         con = con.gsub(/[^a-zA-Z0-9-]/, " ")
-        pdf.text "<b>Due Date</b>" + ":" + "" + "#{homework.due_date.strftime("%B %d %Y")}", :font_size => 10, :justification => :left
+        pdf.text "<b>Due Date</b>" + ":" + "" + "#{homework.due_date.strftime("%B %d %Y")}", :font_size => 10, :justification => :left, :spacing => 2
         pdf.text "<b>Title</b>" + ":" + "" + "#{homework.title}", :font_size => 10, :justification => :left
         pdf.text "<b>Description</b>" + ":" + "" + con, :font_size => 10, :justification => :left
       end
