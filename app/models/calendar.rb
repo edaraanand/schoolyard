53e1fe9cb67f916c89b09e88cb8cb27d1ff66c8b
Eshwar <raja@eshwars-mac-mini.local>
2009-09-03 22:45:05 +0530

    [fix] added validation for calendars

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -15,7 +15,7 @@ class Calendar < ActiveRecord::Base
       self.errors.add(:end_date, "must be after the start date") if self.end_date < self.start_date
     end
     if ((self.day_event != true) && ((self.end_time != nil) && (self.start_time != nil)) )
-       self.errors.add(:end_time, "must be greater than start time") if self.end_time <= self.start_time
+       self.errors.add(:end_time, "must be greater than start time") if ((self.end_time <= self.start_time) && (self.end_date <= self.start_date))
     end
     if self.class_name == ""
        self.errors.add(:class_name, "must be selected")
