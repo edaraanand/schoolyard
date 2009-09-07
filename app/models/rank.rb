c81bce435bcda0d3efe12a96cbe32376353ad27b
Eshwar <raja@eshwars-mac-mini.local>
2009-09-07 16:58:20 +0530

    [fix] Misc changes in grades

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -5,7 +5,7 @@ class Rank < ActiveRecord::Base
   belongs_to :assignment
   
  # validates_presence_of :from, :to, :name
-#  validates_uniqueness_of :from, :to, :scope => :school_id
-  
+ #  validates_uniqueness_of :from, :to, :scope => :school_id
+ 
  
 end
