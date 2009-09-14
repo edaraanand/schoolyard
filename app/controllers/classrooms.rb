a145e96a65454111fe2a9eb10fd75a00af3445b5
Eshwar <raja@eshwars-mac-mini.local>
2009-09-14 12:36:19 +0530

    [fix] solved the issue for expiration of announcements in classrooms page

--- app/controllers/classrooms.rb
+++ app/controllers/classrooms.rb
@@ -122,21 +122,21 @@ class Classrooms < Application
     @classroom = @current_school.classrooms.find_by_id(params[:id])
     if @classroom
       if @classroom.activate == true
-         #@calendars = @current_school.calendars.paginate(:all, :conditions => ['class_name = ?', @classroom.class_name], 
-                                                       #  :per_page => 10, :page => params[:page], :order => 'start_date DESC')
          @all_class_calendars = Calendar.all_calendars(@current_school.id)
          @cals = @current_school.calendars.find(:all, :conditions => ["class_name = ?", @classroom.class_name])
          @calendars = @cals.concat(@all_class_calendars).sort_by{|my_item| my_item[:start_date]}.uniq
-         @announcements = @current_school.announcements.paginate(:all, :conditions => ["access_name = ? and approved = ? and approve_announcement = ?", @classroom.class_name, true, true], :per_page => 10,
-                                                              :page => params[:page], :order => "created_at DESC")
+         @announcements = @current_school.announcements.paginate(:all, :conditions => ["access_name = ? and approved = ? and approve_announcement = ?", @classroom.class_name, true, true],
+                                           :per_page => 10, :page => params[:page], :order => "created_at DESC")
          @welcome_messages = @current_school.welcome_messages.find(:all, :conditions => ['access_name = ?', @classroom.class_name])
          @external_links = @current_school.external_links.find(:all, :conditions => ["label = ? and classroom_id = ?", "Classrooms", @classroom.id])
          @subject_links = @current_school.external_links.find(:all, :conditions => ["label = ? and classroom_id = ?", "Subjects", @classroom.id])
          @spot_lights = @current_school.spot_lights.paginate(:all, :conditions => ['class_name = ?', @classroom.class_name], :per_page => 2, :page => params[:page], :order => 'created_at DESC')
-         @ann = @current_school.announcements.find(:all, :conditions => ["access_name = ? and approved = ? and approve_announcement = ?", @classroom.class_name, true, true], :limit => 3, :order => "created_at DESC")
+         @ann = @current_school.announcements.find(:all, :conditions => ["access_name = ? and approved = ? and approve_announcement = ? and expiration >= ?", @classroom.class_name, true, true, @date], 
+                                                   :limit => 3, :order => "created_at DESC")
          @sp_light = @current_school.spot_lights.find(:first, :conditions => ['class_name = ?', @classroom.class_name], :order => "created_at DESC" )
          unless @classroom.class_type == "Subject"
-           @home_works = @classroom.home_works.paginate(:all, :conditions => ['school_id = ?', @current_school.id], :order => "due_date ASC", :per_page => 10, :page => params[:page])
+           @home_works = @classroom.home_works.paginate(:all, :conditions => ['school_id = ?', @current_school.id], :order => "due_date ASC", 
+                                                        :per_page => 10, :page => params[:page])
            @h_w = @classroom.home_works.find(:all, :conditions => ['school_id = ? and due_date >= ?', @current_school.id, @date], :order => "due_date ASC" )
            @reports = @current_school.reports.find(:all, :conditions => ['classroom_id = ?', @classroom.id])
            @ranks = @current_school.ranks.find(:all)
