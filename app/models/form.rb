6960f5d57f16bde523ed45021951db3e6c08fdec
Eshwar <raja@eshwars-mac-mini.local>
2009-09-17 20:05:42 +0530

    [fix] modified the code for forms functionality

--- app/models/form.rb
+++ app/models/form.rb
@@ -5,5 +5,15 @@ class Form < ActiveRecord::Base
   belongs_to :school
 
   validates_presence_of :title, :description
+   
+  def validate
+    if self.class_name == ""
+       self.errors.add("please", "select the class")
+    end
+  end
+  
+  def self.all_forms(id)
+    self.find(:all, :conditions => ["class_name = ? and school_id = ?", "All Classes", id ])
+  end
 
 end
