1b6229c0b991f3128efcf0a10266d44f45f0de19
Brian <brian@insightmethods.com>
2009-09-22 11:52:25 -0700

    [FIX] refactored messaging for urgent alerts

--- app/controllers/notifications.rb
+++ app/controllers/notifications.rb
@@ -19,41 +19,19 @@ class Notifications < Application
   
   def create
      @announcement = @current_school.announcements.new(params[:announcement])    
-     if @announcement.valid?   
-        sms_numbers = []
+     if @announcement.valid?
         @announcement.access_name = "Home Page"
         @announcement.label = "urgent"
         content = format(params[:announcement][:content])
         @announcement.content = content
         @announcement.save!
-        makecall(@announcement.id)
-        run_later do
-           post_announcement(@announcement.id)
-        end
+
+        post_urgent_announcement_later(@announcement.id)
+
         redirect resource(:homes)
      else
         render :new                
      end 
-  end
-  
-  def post_announcement(id)
-    @announcement = @current_school.announcements.find_by_id(params[:id]) rescue NotFound   
-    twitter = Twitter.new(@current_school.username, @current_school.password)
-    twitter.post(@announcement.content)
-    numbers = @current_school.people.find(:all).collect{ |x| x.sms_alert }.compact.delete_if{|x| x == ""}.collect{|x| "1" + x }
-    api = Clickatell::API.authenticate(Schoolapp.config(:clickatell_api), Schoolapp.config(:clickatell_user), Schoolapp.config(:clickatell_password))
-    unless numbers.empty?
-      numbers.each do |f|
-         msg_code = api.send_message("#{f}", "#{@announcement.content}")
-         n = f.split(',').flatten
-         i = n[0].split('')
-         l = i.shift
-         sms_numbers << "#{i.join()}"
-         @person = @current_school.people.find_by_sms_alert("#{i.join()}")
-         LoggerMachine.create({:person_id => @person.id, :school_id => @current_school.id, :announcement_id => @announcement.id, :sms_code => "#{msg_code}"})  
-      end
-    end
-    @announcement.mail(:urgent_announcement, :subject => "Urgent Announcement for " + @current_school.school_name)
   end
   
   def show
@@ -102,28 +80,6 @@ class Notifications < Application
     raise NotFound unless @announcement
     @announcement.destroy
     redirect url(:homes)
-  end
-  
-  def makecall(id)
-    @announcement = @current_school.announcements.find_by_id(id)
-    @people = @current_school.people.find(:all)
-    @people.each do |f|
-       unless f.voice_alert.blank?
-          begin  
-            account = TwilioRest::Account.new( Schoolapp.config(:twilio_account_id), Schoolapp.config(:twilio_account_token))
-            resp =  account.request("/#{Schoolapp.config(:twilio_api_version)}/Accounts/#{Schoolapp.config(:twilio_account_id)}/Calls.csv", 'POST',
-                                          d = { 'Caller' => Schoolapp.config(:caller_id),
-                                                'Called' => "#{f.voice_alert}",
-                                                'Url' => "http://#{@current_school.subdomain}.#{Schoolapp.config(:app_domain)}" + "/reminder?id=#{@announcement.id}" })
-            sID = resp.body.gsub("\n", ",").split(",")
-            LoggerMachine.create({:person_id => f.id, :school_id => @current_school.id, :twilio_call_id => sID[15], :announcement_id => @announcement.id})                                     
-            resp.error! unless resp.kind_of? Net::HTTPSuccess  
-          rescue StandardError => bang
-            return  
-          end  
-       end
-    end
-    redirect resource(:notifications)
   end
    
   def reminder  
