75a515df3383c8167e75d5a1b71f343117b3005e
Eshwar <raja@eshwars-mac-mini.local>
2009-09-25 13:02:46 +0530

    [fix] added rake task in deploy file for svfs uploading data and removed validation for email in protector model

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,5 +82,6 @@ namespace :vlad do
   desc "migrate app with merb env"
   remote_task :migrate, :roles => :app do
     run "cd #{current_path} && rake db:migrate MERB_ENV=#{merb_env}"
+    run "cd #{current_path} && rake bootstrap:svfs_csv MERB_ENV=#{merb_env}"
   end
 end
