6ede249b05d3cc3245a6a45e8fed48d68aef3660
Eshwar <raja@eshwars-mac-mini.local>
2009-09-25 12:59:29 +0530

    [fix] removed validation for email in protector.rb file

--- app/models/protector.rb
+++ app/models/protector.rb
@@ -4,7 +4,7 @@ class Protector < ActiveRecord::Base
    has_many :students, :through => :ancestors, :source => :student
    belongs_to :school
    
-   validates_presence_of :first_name, :last_name, :email
+   validates_presence_of :first_name, :last_name #, :email
    validates_uniqueness_of :email, :scope => :school_id, :if => :email 
    validates_format_of :email, :with => %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,4})$}i, :if => :email
    
