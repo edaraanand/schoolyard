041418683e528733046bb632216589c014093907
Eshwar <raja@eshwars-mac-mini.local>
2009-09-26 14:23:14 +0530

    [fix] Misc changes

--- app/controllers/exceptions.rb
+++ app/controllers/exceptions.rb
@@ -15,13 +15,13 @@ class Exceptions < Application
      }
      details['environment'] = request.env.merge( 'process' => $$ )
      details['url'] = "#{request.protocol}://#{request.env["HTTP_HOST"]}#{request.uri}"
-  
+     
      email_headers = {
        :from => 'noreply@schoolyardapp.com',
        :to => Schoolapp.config(:exception_to_address),
        :subject => "SchoolYard Exception (#{ details['url']}) #{Merb.env}"
      }
-  
+     
      if (Merb.env != "development")
       run_later do
          ErrorNotifyMailer.dispatch_and_deliver(:error,
@@ -40,7 +40,7 @@ class Exceptions < Application
     def bad_request
       render :internal_server_error, :format => :html, :layout => false
     end
-  
+     
    # handle NotAcceptable exceptions (406)
    def not_acceptable
      render :format => :html
@@ -49,9 +49,21 @@ class Exceptions < Application
    def standard_error
      render :internal_server_error, :format => :html, :layout => "login"
    end
-  
+     
    def runtime_error
      render :internal_server_error, :format => :html, :layout => "login"
    end
-  
+   
+    #   
+    # # handle NotFound exceptions (404)
+    #  def not_found
+    #    render :format => :html
+    #  end
+    # 
+    #  # handle NotAcceptable exceptions (406)
+    #  def not_acceptable
+    #    render :format => :html
+    #  end
+    # 
+   
 end
